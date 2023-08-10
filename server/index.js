1d0a5c4c66fb9b54de72ad1403723f864c578081
k2day <ky2day11@gmail.com>
2023-08-09 19:41:17 -0600

    Trying to fix branch stuff

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log('test')
+import express from 'express';
+import session from 'express-session';
+
+const app = express();
+
+app.use(session({
+    secret: 'this is a very secret code',
+    saveUninitialized: true,
+    resave: false
+}));
+
+app.set("/", )
+
+app.listen(3000);
---
+++ server/routes/authenticationRoutes.js
@@ -0,0 +1,33 @@
+import bcrypt from 'bcryptjs';
+
+const createAccount = (req, res) =>{
+    let salt = bcrypt.genSaltSync(10);
+    let data ={
+        email: req.body.email,
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password, salt)
+    }
+
+    res.redirect('/');
+};
+
+const login = (req, res) =>{
+    let salt = bcrypt.genSaltSync(10);
+    let userPassword = bcrypt.hashSync("password", salt); //replace with user password
+    //find user from database by username = req.body.username
+
+    if(bcrypt.compareSync(req.body.password, userPassword)){
+        req.session.user = {
+            isAuthenticated: true,
+            //use user info for below
+            username: "user",
+            id: 1
+        }
+    }
+    res.redirect('/');
+};
+
+export {
+    createAccount,
+    login
+}
