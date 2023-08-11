80186903eae345a8e71d175a6b183a33ae50b902
k2day <ky2day11@gmail.com>
2023-08-10 20:30:18 -0600

    auth finished, untested.

--- server/routes/authenticationRoutes.js
+++ server/routes/authenticationRoutes.js
@@ -1,32 +1,42 @@
-import bcrypt from 'bcryptjs';
+// import bcrypt from 'bcryptjs';
+const bcrypt = require('bcryptjs');
+// import mongo from '../mongo/account.mongo.js';
+const mongo = require('../mongo/account.mongo.js');
 
 const createAccount = (req, res) =>{
+    mongo.Get({email: req.body.email},()=>{
+        res.sendStatus(400)
+    })
+
     let salt = bcrypt.genSaltSync(10);
-    let data ={
+    let user ={
         email: req.body.email,
-        username: req.body.username,
         password: bcrypt.hashSync(req.body.password, salt)
     }
-    res.sendStatus(200);
+
+    mongo.Post(user, ()=>{
+        req.session.user = {
+            isAuthenticated: true,
+            email: user.email
+        }
+        res.sendStatus(200);
+    });
 };
 
 const login = (req, res) =>{
-    let salt = bcrypt.genSaltSync(10);
-    let userPassword = bcrypt.hashSync("password", salt); //replace with user password
-    //find user from database by username = req.body.username
-
-    if(bcrypt.compareSync(req.body.password, userPassword)){
-        req.session.user = {
-            isAuthenticated: true,
-            //use user info for below
-            username: "user",
-            id: 1
+    mongo.Get({email:req.body.email},(user)=>{
+        if(bcrypt.compareSync(req.body.password, user.password)){
+            req.session.user = {
+                isAuthenticated: true,
+                email: user.email
+            }
+            res.sendStatus(200)
         }
-    }
-    res.redirect('/');
+    })
+    res.sendStatus(400)
 };
 
-export {
+module.exports = {
     createAccount,
     login
 }
