64e33d4b3081fed456408a0b9c652ce752bfa43a
RatRyan <ryantratajczak@gmail.com>
2023-08-11 23:38:11 -0600

    remade vue client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <h1>Mal-Wares</h1>
-  <h2>An e-commerce site for real products</h2>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
