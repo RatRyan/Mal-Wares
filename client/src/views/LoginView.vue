10e3efb3bd7ca22ace5730dfc454c48b686ac98e
RatRyan <ryantratajczak@gmail.com>
2023-08-13 01:30:22 -0600

    fininshed login/register pages

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,3 +1,26 @@
 <template>
-  
+  <div
+    class="d-flex flex-column min-vh-100 justify-content-center align-items-center"
+  >
+    <div class="card d-flex flex-column justify-content-center">
+      <h1>Login</h1>
+      <form class="d-flex flex-column mt-3">
+        <label for="email">Email Address</label>
+        <input class="form-control" type="email" id="email" required />
+        <label for="password">Password</label>
+        <input class="form-control" type="password" id="password" required />
+        <button class="btn btn-primary mt-3 mb-3" type="button">Login</button>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script>
+import { RouterLink } from "vue-router";
+</script>
+
+<style scoped>
+.card {
+  padding: 2rem;
+}
+</style>
