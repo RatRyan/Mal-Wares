d9f30a86a8437f43da924463cd7c9cc27c4964ea
RatRyan <ryantratajczak@gmail.com>
2023-08-12 20:11:47 -0600

    created basic routes

---
+++ client/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: "/register",
+      name: "Register",
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: "/products/:productId",
+      name: "product",
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: "/cart",
+      name: "Cart",
+      component: () => import('../views/CartView.vue')
+    },
+  ]
+})
