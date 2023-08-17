a27242ee38e2734255f2654d2b9e94f7f1018971
RatRyan <ryantratajczak@gmail.com>
2023-08-16 18:44:03 -0600

    reworked login/register

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,34 +1,49 @@
 <template>
-  <div
-    class="d-flex flex-column min-vh-100 justify-content-center align-items-center"
-  >
-    <div class="card d-flex flex-column justify-content-center">
-      <h1>Login</h1>
-      <form class="d-flex flex-column mt-3">
-        <label for="email">Email Address</label>
-        <input
-          class="form-control"
-          type="email"
-          id="email"
-          placeholder="email"
-          required
-        />
-        <label for="password">Password</label>
-        <input
-          class="form-control"
-          type="password"
-          id="password"
-          placeholder="password"
-          required
-        />
-        <button class="btn btn-primary mt-3 mb-3" type="button">Login</button>
+  <div class="d-flex flex-column justify-content-center min-vh-100">
+    <div class="login-form container bg-white">
+      <form class="d-flex flex-column">
+        <h2>Login</h2>
+        <label>Email:</label>
+        <div class="input-group">
+          <span class="input-group-text">
+            <i class="bi bi-envelope" />
+          </span>
+          <input class="form-control border-start-0" type="email" id="email" />
+        </div>
+        <label>Password:</label>
+        <div class="input-group">
+          <span class="input-group-text"
+            ><i class="bi bi-shield-lock"></i
+          ></span>
+          <input
+            class="form-control border-start-0"
+            type="password"
+            id="password"
+          />
+        </div>
+        <button class="btn btn-primary mt-5">Login</button>
+        <hr>
+        <RouterLink to="/register">Create an account</RouterLink>
       </form>
     </div>
   </div>
 </template>
 
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
 <style scoped>
-.card {
-  padding: 2rem;
+.login-form {
+  max-width: 450px;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px 2px rgb(118, 118, 118);
+}
+a {
+  text-decoration: none;
+}
+h2:hover {
+  color: blue;
 }
 </style>
--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,34 +1,75 @@
 <template>
-  <div
-    class="d-flex flex-column min-vh-100 justify-content-center align-items-center"
-  >
-    <div class="card d-flex flex-column justify-content-center">
-      <h1>Register</h1>
-      <form class="d-flex flex-column mt-3">
-        <label for="email">Email Address</label>
-        <input
-          class="form-control"
-          type="email"
-          id="email"
-          placeholder="email"
-          required
-        />
-        <label for="password">Password</label>
-        <input
-          class="form-control"
-          type="password"
-          id="password"
-          placeholder="password"
-          required
-        />
-        <button class="btn btn-primary mt-3 mb-3" type="button">Register</button>
+  <div class="d-flex flex-column justify-content-center min-vh-100">
+    <div class="login-form container bg-white">
+      <form class="d-flex flex-column">
+        <h2>Register</h2>
+        <div class="d-flex">
+          <div class="name-field">
+            <label>First Name:</label>
+            <div class="input-group">
+              <span class="input-group-text">
+                <i class="bi bi-person" />
+              </span>
+              <br />
+              <input
+                class="form-control border-start-0"
+                type="email"
+                id="email"
+              />
+            </div>
+          </div>
+          <div class="name-field mr-1">
+            <label>Last Name:</label>
+            <input
+              class="form-control border-start-0"
+              type="email"
+              id="email"
+            />
+          </div>
+        </div>
+        <label>Email:</label>
+        <div class="input-group">
+          <span class="input-group-text">
+            <i class="bi bi-envelope" />
+          </span>
+          <input class="form-control border-start-0" type="email" id="email" />
+        </div>
+        <label>Password:</label>
+        <div class="input-group">
+          <span class="input-group-text"
+            ><i class="bi bi-shield-lock"></i
+          ></span>
+          <input
+            class="form-control border-start-0"
+            type="password"
+            id="password"
+          />
+        </div>
+        <button class="btn btn-primary mt-5">Register</button>
+        <hr>
+        <div class="d-flex">
+          <RouterLink to="/login">Already have an account? Login</RouterLink>
+        </div>
       </form>
     </div>
   </div>
 </template>
 
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
 <style scoped>
-.card {
-  padding: 2rem;
+.name-field {
+  width: 50%;
+}
+.login-form {
+  max-width: 450px;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px 2px rgb(118, 118, 118);
+}
+a {
+  text-decoration: none;
 }
 </style>
