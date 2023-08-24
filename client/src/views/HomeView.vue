4934c8160e65515b46445d9b532c7980e2d18a1c
RatRyan <ryantratajczak@gmail.com>
2023-08-24 10:41:10 -0600

    created homepage structure

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <template>
   <Navbar></Navbar>
-  <Product product-name="test product"/>
+  <div v-for="product in products" class="container-sm">
+    <Product
+      :product-name="product.name"
+      :product-description="product.description"
+      :product-price="product.price"
+    >
+    </Product>
+  </div>
 </template>
 
 <script setup>
 import Navbar from '../components/Navbar.vue';
 import Product from '../components/Product.vue';
+import { ref } from 'vue';
 
+const products = ref([]);
 </script>
