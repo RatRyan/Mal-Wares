1711c3f02be1b56ee6eda301122c3a0615f96742
RatRyan <ryantratajczak@gmail.com>
2023-08-23 21:50:21 -0600

    fixed navbar import issue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="d-flex flex-column vh-100">
-    <NavBar></NavBar>
-    <div class="d-flex flex-grow-1">
-      <RouterView />
+    <div class="d-flex flex-column vh-100">
+        <NavBar></NavBar>
+        <div class="d-flex flex-grow-1">
+            <RouterView />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
-import NavBar from './components/NavBar.vue';
+import NavBar from './components/Navbar.vue';
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
