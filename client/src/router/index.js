37b0acd272860c3c7207fed0d319be51761d5a8a
RatRyan <ryantratajczak@gmail.com>
2023-08-26 18:35:38 -0600

    added route protection to the account vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,31 +1,42 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
+import { useUserStore } from '../stores/UserStore';
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/",
-      component: () => import("../views/HomeView.vue"),
+      path: '/',
+      component: () => import('../views/HomeView.vue'),
     },
     {
-      path: "/register",
-      component: () => import("../views/RegisterView.vue"),
+      path: '/register',
+      component: () => import('../views/RegisterView.vue'),
     },
     {
-      path: "/login",
-      component: () => import("../views/LoginView.vue"),
+      path: '/login',
+      component: () => import('../views/LoginView.vue'),
     },
     {
-      path: "/account",
-      component: () => import("../views/AccountView.vue"),
+      path: '/account',
+      component: () => import('../views/AccountView.vue'),
+      beforeEnter: (to, from, next) => {
+        const userStore = useUserStore();
+        if (!userStore.loggedIn) {
+          next({ path: '/login' });
+        } else {
+          next();
+        }
+      },
     },
     {
-      path: "/products/:productId",
-      component: () => import("../views/HomeView.vue"),
+      path: '/products/:productId',
+      component: () => import('../views/HomeView.vue'),
     },
     {
-      path: "/cart",
-      component: () => import("../views/CartView.vue"),
+      path: '/cart',
+      component: () => import('../views/CartView.vue'),
     },
   ],
 });
+
+export default router;
